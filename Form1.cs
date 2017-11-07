eb3b40f7eedfce7c5a5ef10f09df6b3b479e7889
wamio001 <33464612+wamio001@users.noreply.github.com>
2017-11-07 14:25:35 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GameLibrary;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace GameFactory
+{
+    public partial class Form1 : Form
+    {
+        StreamWriter sw;
+        XmlSerializer serial;
+        List<Game> gameList;
+        const string Games_FileName = @"..\..\games.xml";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gameList = new List<Game>();
+            Game g = new Game("Team1", "Team2", 10, 20);
+            gameList.Add(g);
+            //label2.Text = g.ToString();
+
+            g = new Game("Eagles", "Bears", 5, 10);
+            gameList.Add(g);
+
+            g = new Game("Crabs", "Deers", 1, 2);
+            gameList.Add(g);
+
+            g = new Game("Hawks", "Tigers", 42, 22);
+            gameList.Add(g);
+
+            g = new Game("Bucks", "Wolves", 30, 25);
+            gameList.Add(g);
+
+            g = new Game("Raptors", "Wizards", 12, 21);
+            gameList.Add(g);
+
+            serial = new XmlSerializer(gameList.GetType());
+            //sw = new StreamWriter(Games_FileName); 
+            serial.Serialize(sw, gameList);
+            sw.Close();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sw = new StreamWriter(Games_FileName);
+            gameList = new List<Game>();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gameList = new List<Game>();
+            StreamReader sr = new StreamReader(Games_FileName);
+            serial = new XmlSerializer(gameList.GetType());
+            gameList = (List<Game>)serial.Deserialize(sr);
+            sr.Close();
+
+            foreach(Game g in gameList)
+            {
+                listBox1.Items.Add(g.ToString());
+            }
+
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary
+{
+    public class Game
+    {
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+        public int Team1Score { get; set; }
+        public int Team2Score { get; set; }
+
+        public Game() { }
+        
+
+        public Game(string Team1, string Team2, int Team1Score, int Team2Score)
+        {
+            this.Team1 = Team1;
+            this.Team2 = Team2;
+            this.Team1Score = Team1Score;
+            this.Team2Score = Team2Score;
+        }
+
+        public override string ToString()
+        {
+            return (Team1+" ("+Team1Score+") - "+Team2+" ("+Team2Score+")");
+        }
+
+    }
+}
